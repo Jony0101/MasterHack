091984da2495f6b26e35fd458ae05ebec6193c62
Jony0101 <121823452+Jony0101@users.noreply.github.com>
2023-01-02 18:53:43 +0000

    Update masterhack.meta.js

--- release/masterhack.meta.js
+++ release/masterhack.meta.js
@@ -1,14 +1,14 @@
 // ==UserScript==
-// @name         Mega Hack JS
+// @name         Master Hack JS
 // @version      1.00.0
 // @description  Tanki Online Cheat
-// @author       Zorro
+// @author       Jony
 // @match        https://*.tankionline.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=tankionline.com
 // @description  Tanki Online Cheat
 
 // @downloadURL  https://github.com/Jony0101/MasterHack/raw/main/release/masterhack.user.js
-// @updateURL    https://github.com/UUID1606/MegaHack/raw/main/release/MEGAHACK.meta.js
+// @updateURL    https://github.com/Jony0101/MasterHack/raw/main/release/masterhack.meta.js
 
 // @run-at       document-start
 // @grant        GM_xmlhttpRequest
