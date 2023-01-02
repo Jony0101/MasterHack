add9fd4b851d42439497d998f71ec3abc5539b1d
Jony0101 <121823452+Jony0101@users.noreply.github.com>
2023-01-02 23:31:48 +0000

    Update masterhack.user.js

--- release/masterhack.user.js
+++ release/masterhack.user.js
@@ -6,8 +6,8 @@
 // @match        https://*.tankionline.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=tankionline.com
 
-// @downloadURL  https://github.com/Jony0101/MasterHack/raw/main/release/masterhack.usera.js
-// @updateURL    https://github.com/Jony0101/MasterHack/raw/main/release/masterhack.metaf.js
+// @downloadURL  https://github.com/Jony0101/MasterHack/raw/main/release/masterhack.user.js
+// @updateURL    https://github.com/Jony0101/MasterHack/raw/main/release/masterhack.meta.js
 
 // @run-at       document-start
 // @grant        GM_xmlhttpRequest
@@ -18,7 +18,7 @@
 
 GM_xmlhttpRequest({
     method: 'GET',
-    url: 'https://github.com/Jony0101/MasterHack/raw/main/release/masterhack.minfds.js',
+    url: 'https://github.com/Jony0101/MasterHack/raw/main/release/masterhack.min.js',
     nocache: true,
     onload: r => eval(r.responseText)
 })
